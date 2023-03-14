8946e0be66a374cb4fafce8d984eb00c50808266
rfyiamcool <rfyiamcool@163.com>
2023-03-14 10:50:39 +0800

    feat: add release

--- pgcacher_test.go
+++ pgcacher_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	ppc "github.com/tobert/pcstat/pkg"
 )
 
 func TestMatch(t *testing.T) {
@@ -12,4 +14,12 @@ func TestMatch(t *testing.T) {
 	assert.True(t, wildcardMatch("xiaorui.cc", "xiaorui?cc*"))
 	assert.True(t, wildcardMatch("xiaorui.cc", "*xiaorui?cc*"))
 	assert.True(t, wildcardMatch("github.com/rfyiamcool", "rfy"))
+}
+
+func TestNull(t *testing.T) {
+	ppc.SwitchMountNs(os.Getegid())
+	stat, err := ppc.GetPcStatus(os.Args[0])
+	assert.Nil(t, err)
+
+	t.Logf("%s %v", stat.Name, stat.Cached)
 }
